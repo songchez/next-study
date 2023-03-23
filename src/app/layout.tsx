501838b19f57c7c8f19657305cb944fe24fb0a07
songchez <tama4840@gmail.com>
2023-03-24 00:01:02 +0900

    4.2까지

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,10 @@ import styles from "./layout.module.css";
 //head.js가 없어지고 오지는 SEO를 위한 metadata가 생겼다!!!!
 //쉽게 동적메타데이타도 만들어 볼 수 있다!
 export const metadata = {
-  title: "산체스의 넥스트 스터디",
+  title: {
+    default: "산체스의 넥스트 스터디",
+    template: "산체스의 넥스트 스터디 | %s",
+  },
   description: "산체스의 넥스트 스터디를 담은 홈페이지",
 };
 
