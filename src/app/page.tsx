5d68afa2fc5087fd583e60a72607a73893a8812f
songchez <tama4840@gmail.com>
2023-03-30 15:29:52 +0900

    Sanity-deploy-test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const client = createClient({
   useCdn: false,
 });
 
-export async function getPostData() {
+async function getPostData() {
   const posts = await client.fetch(`*[_type == "post"]`);
   if (posts === null) {
     throw new Error("데이터를 가져오는데 실패!");
